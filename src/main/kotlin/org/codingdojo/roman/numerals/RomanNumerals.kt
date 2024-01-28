6cb9441032f93bba88c990e2e5226ef7b901f28e
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2024-01-28 22:29:46 +0100

    [Roman-Numerals] convert 1

---
+++ src/main/kotlin/org/codingdojo/roman/numerals/RomanNumerals.kt
@@ -0,0 +1,8 @@
+package org.codingdojo.roman.numerals
+
+class RomanNumerals {
+    fun convert(i: Int): String {
+        return "I";
+    }
+
+}
--- src/test/kotlin/org/codingdojo/roman/numerals/RomanNumeralsTest.kt
+++ src/test/kotlin/org/codingdojo/roman/numerals/RomanNumeralsTest.kt
@@ -1,13 +1,13 @@
 package org.codingdojo.roman.numerals
 
-import org.amshove.kluent.shouldBeTrue
+import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 
 class RomanNumeralsTest {
 
     @Test
-    fun `test`() {
-        true.shouldBeTrue()
+    fun `should convert 1 to I`() {
+        RomanNumerals().convert(1) `should be equal to` "I";
     }
 
 }
