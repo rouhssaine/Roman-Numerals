800e1b9307c241a8dbd48a74f6fad1f39a91fc4f
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2024-01-28 22:29:46 +0100

    [Roman-Numerals] convert final

--- src/test/kotlin/org/codingdojo/roman/numerals/RomanNumeralsTest.kt
+++ src/test/kotlin/org/codingdojo/roman/numerals/RomanNumeralsTest.kt
@@ -15,9 +15,19 @@ class RomanNumeralsTest {
             "3 ➔ III",
             "4 ➔ IV",
             "5 ➔ V",
+            "9 ➔ IX",
             "10 ➔ X",
             "20 ➔ XX",
-            "30 ➔ XXX"
+            "21 ➔ XXI",
+            "30 ➔ XXX",
+            "40 ➔ XL",
+            "50 ➔ L",
+            "90 ➔ XC",
+            "100 ➔ C",
+            "400 ➔ CD",
+            "500 ➔ D",
+            "900 ➔ CM",
+            "1000 ➔ M"
         ]
     )
     fun should_convert(number: Int, romanNumber: String) {
